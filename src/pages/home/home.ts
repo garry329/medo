23a4ab74bc70e2bde83c09952b60c93b934bd12f
danytargaryen <itssmartashutosh@gmail.com>
2018-03-11 02:09:10 +0530

    :slider-added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Angular2TokenService, A2tUiModule } from 'angular2-token';
+import { Slides } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(Slides) slides: Slides;
 
   constructor(private _tokenService: Angular2TokenService,public navCtrl: NavController) {
   	this._tokenService.init(
   		{
   			registerAccountPath:'/auth'
   		});
+  }
+
+  slideChanged() {
+    let currentIndex = this.slides.getActiveIndex();
+    console.log('Current index is', currentIndex);
+  }
+
+  ionViewWillEnter(){
+    // global.setTimeout(function(){
+    //   // this.slides.slideNext(500, true)
+    //   console.log("hello")
+    // },1000);
+    
   }
 
 signUp(){
