4f25defe92c58a2ece12a7abb581e6399465e3dd
garry329 <jaideep329@gmail.com>
2018-03-11 12:10:58 +0530

    testing api post

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -19,14 +19,19 @@ import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 export class SignUpPage {
 	email:string;
 	password:string;
+	output: any;
 	passwordConfirmation:string;
 	private registerForm : FormGroup;
 	registerData: RegisterData = <RegisterData>{};
-  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController, private formBuilder: FormBuilder) {
+  constructor(private _tokenService: Angular2TokenService,public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController, private formBuilder: FormBuilder) {
   	this.registerForm = this.formBuilder.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
       passwordConfirmation: ['',Validators.required]
+    });
+
+    this._tokenService.init({
+    	registerAccountPath: 'https://localhost:3000/auth'
     });
   }
 
@@ -37,19 +42,22 @@ export class SignUpPage {
   	
 
 	signUp(){
-		// this._tokenService.registerAccount({
-		//     email:  this.email,
-		//     password: this.password,
-		//     passwordConfirmation: this.passwordConfirmation
-		// }).subscribe(
-		//     res =>      console.log(res),
-		//     error =>    console.log(error)
-		// );
-		console.log(this.registerData.email)
-		console.log(this.registerData.password)
-		console.log(this.registerData.passwordConfirmation)
-		this.showAlert('bewakoof','Password Sahi Daal Dia kar')
-		this.navCtrl.push(HomePage);
+		this.output = null;
+
+        this._tokenService.registerAccount(this.registerData).subscribe(
+            res => {
+                this.registerData  = <RegisterData>{};
+                this.output        = res;
+            }, error => {
+                this.registerData  = <RegisterData>{};
+                this.output        = error;
+            }
+        );
+		// console.log(this.registerData.email)
+		// console.log(this.registerData.password)
+		// console.log(this.registerData.passwordConfirmation)
+		// this.showAlert('bewakoof','Password Sahi Daal Dia kar')
+		// this.navCtrl.push(HomePage);
 		
 	}
 
